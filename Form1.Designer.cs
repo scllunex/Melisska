e5f3aaabed20d39c06e1b807300c318c946ea08d
serggys <52373427+serggys@users.noreply.github.com>
2025-05-18 22:36:04 +0300

    Привет

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,141 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.clickButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.clickBut = new System.Windows.Forms.Button();
+            this.настройкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clickButton
+            // 
+            this.clickButton.Location = new System.Drawing.Point(364, 168);
+            this.clickButton.Name = "clickButton";
+            this.clickButton.Size = new System.Drawing.Size(100, 100);
+            this.clickButton.TabIndex = 0;
+            this.clickButton.Text = "Click me!!!";
+            this.clickButton.UseVisualStyleBackColor = true;
+            this.clickButton.Click += new System.EventHandler(this.ClickButton);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(248, 212);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(39, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Счет 0";
+            this.scoreLabel.Click += new System.EventHandler(this.ScoreLabel_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкаToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem1.Text = "Меню";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(248, 243);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(45, 13);
+            this.timeLabel.TabIndex = 5;
+            this.timeLabel.Text = "Time 10";
+            // 
+            // clickBut
+            // 
+            this.clickBut.Image = ((System.Drawing.Image)(resources.GetObject("clickBut.Image")));
+            this.clickBut.Location = new System.Drawing.Point(519, 93);
+            this.clickBut.Name = "clickBut";
+            this.clickBut.Size = new System.Drawing.Size(260, 251);
+            this.clickBut.TabIndex = 6;
+            this.clickBut.UseVisualStyleBackColor = true;
+            this.clickBut.Click += new System.EventHandler(this.clickButton2_Click);
+            // 
+            // настройкаToolStripMenuItem
+            // 
+            this.настройкаToolStripMenuItem.Name = "настройкаToolStripMenuItem";
+            this.настройкаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.настройкаToolStripMenuItem.Text = "Настройка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickBut);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.clickButton);
+            this.Controls.Add(this.menuStrip);
+            this.Location = new System.Drawing.Point(10, 40);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button clickButton;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button clickBut;
+        private System.Windows.Forms.ToolStripMenuItem настройкаToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,128 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+using Microsoft.VisualBasic;
 
 namespace Melisska
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        private int score = 0;
+        private int timeLeft = 10;
+        private Timer gameTimer;
+        private bool isGameActive = false;
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
         }
-    }
-}
+
+        private void InitializeGame()
+        {
+            gameTimer = new Timer();
+            gameTimer.Interval = 1000; // 1 second
+            gameTimer.Tick += timer1_Tick;
+
+         
+        }
+        private void ClickButton(object sender, EventArgs e)
+        {
+            if (!isGameActive)
+            {
+                isGameActive = true;
+                score = 0;
+                timeLeft = 10;
+                gameTimer.Start();
+            }
+
+            score++;
+            SystemSounds.Beep.Play();
+            UpdateScoreLabel();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            UpdateTimeLabel();
+
+            if (timeLeft <= 0)
+            {
+                gameTimer.Stop();
+                isGameActive = false;
+                MessageBox.Show($"Вы накликали {score}!");
+            }
+        }
+
+        private void ScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateScoreLabel()
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is Label && control.Name == "scoreLabel")
+                {
+                    control.Text = $"Score: {score}";
+                    break;
+                }
+            }
+        }
+
+        private void UpdateTimeLabel()
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is Label && control.Name == "timeLabel")
+                {
+                    control.Text = $"Time: {timeLeft}";
+                    break;
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // Открытие диалогового окна для ввода времени
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Введите время игры в секундах:", "Настройки времени", "10");
+            if (int.TryParse(input, out int newTime))
+            {
+                timeLeft = newTime;
+                UpdateTimeLabel();
+            }
+        }
+
+  
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            // Обработка нажатия клавиши пробела
+            if (e.KeyCode == Keys.Space)
+            {
+                score++;
+                SystemSounds.Beep.Play();
+                UpdateScoreLabel();
+            }
+        }
+        private void TimeSettingsMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Введите время игры в секундах:", "Настройки времени", "10");
+            if (int.TryParse(input, out int newTime))
+            {
+                timeLeft = newTime;
+                UpdateTimeLabel();
+            }
+        }
+
+        private void clickButton2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это Вы!");
+        }
+    } }
